fd75368e5fb0689d3935a61d24287bcfd36d6fe2
Rob@KLPD <rob@klpd>
2011-07-14 13:09:42 +0200

    Moved primitive shell script to a somewhat less primitive makefile.

--- src/pbdns/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#This is a primitive build script. Given that currently Ubuntu 10.4 LTS server 64 bit is our only target platform this should be OK for now.
-#In the future we may want to move the build stuff to the cmake build system.
-g++ main.cpp UnPrivileged.cpp MainServer.cpp PbRoutingCore.cpp DnsForwarder.cpp jsonmeimpl/PbrConfigFactory.cpp jsonmeimpl/JsonMeConfig.cpp jsonmeimpl/JsonMeWsnetConfig.cpp DnsResponse.cpp -lboost_system -lboost_regex -ljsonme -o pbdnsd
-#g++ core_test_main.cpp ./jsonmeimpl/PbrConfigFactory.cpp ./jsonmeimpl/JsonMeConfig.cpp ./jsonmeimpl/JsonMeWsnetConfig.cpp  PbRoutingCore.cpp -I/usr/local/include -ljsonme -lboost_system -o core_test
